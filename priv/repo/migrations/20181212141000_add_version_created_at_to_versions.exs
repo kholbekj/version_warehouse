935685d1b004a41f77e370ace23bd22ca93d667e
Nicolai Seerup <seerup@autobutler.dk>
2018-12-12 15:29:12 +0100

    add update statement to migration

--- priv/repo/migrations/20181212141000_add_version_created_at_to_versions.exs
+++ priv/repo/migrations/20181212141000_add_version_created_at_to_versions.exs
@@ -5,5 +5,12 @@ defmodule VersionWarehouse.Repo.Migrations.AddVersionCreatedAtToVersions do
     alter table(:versions) do
       add :version_created_at, :utc_datetime
     end
+
+    execute """
+      UPDATE versions
+      SET version_created_at = timezone('UTC', created_at::timestamptz)
+      WHERE version_created_at IS NULL
+      AND created_at IS NOT NULL
+    """
   end
 end
