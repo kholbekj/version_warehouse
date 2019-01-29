672865ca3f23350e43f9efd19ce6a0b00059a154
Nicolai Seerup <seerup@autobutler.dk>
2019-01-29 11:31:17 +0100

    Update 20181212141000_add_version_created_at_to_versions.exs

--- priv/repo/migrations/20181212141000_add_version_created_at_to_versions.exs
+++ priv/repo/migrations/20181212141000_add_version_created_at_to_versions.exs
@@ -6,13 +6,6 @@ defmodule VersionWarehouse.Repo.Migrations.AddVersionCreatedAtToVersions do
       add :version_created_at, :utc_datetime
     end
 
-    execute """
-      UPDATE versions
-      SET version_created_at = timezone('UTC', created_at::timestamptz)
-      WHERE version_created_at IS NULL
-      AND created_at IS NOT NULL
-    """
-
     create index(:versions, :version_created_at, concurrently: true)
 
     create index(
