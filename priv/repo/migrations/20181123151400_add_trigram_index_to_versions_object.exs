1e65b4da0f408e80d3d583cf8e200924d0ad1fef
Nicolai Seerup <seerup@autobutler.dk>
2019-01-29 13:04:17 +0100

    Update 20181123151400_add_trigram_index_to_versions_object.exs

--- priv/repo/migrations/20181123151400_add_trigram_index_to_versions_object.exs
+++ priv/repo/migrations/20181123151400_add_trigram_index_to_versions_object.exs
@@ -3,7 +3,7 @@ defmodule VersionWarehouse.Repo.Migrations.AddTrigramIndexToVersionsObject do
   @disable_ddl_transaction true
 
   def up do
-    CREATE EXTENSION pg_trgm IF NOT EXISTS
+    execute("CREATE EXTENSION pg_trgm IF NOT EXISTS")
     execute("""
     CREATE INDEX CONCURRENTLY versions_object_trgm_idx ON versions USING GIN (to_tsvector('english',
       object))
@@ -12,6 +12,6 @@ defmodule VersionWarehouse.Repo.Migrations.AddTrigramIndexToVersionsObject do
 
   def down do
     execute('DROP INDEX versions_object_trgm_idx')
-    DROP EXTENSION pg_trgm IF EXISTS
+    execute("DROP EXTENSION pg_trgm IF EXISTS")
   end
 end
